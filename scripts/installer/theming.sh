1417777e8585bb7b6e4406cdd84bdcb6695966a7
Saltyfunnel <gerardsheal@gmail.com>
2025-07-03 13:08:33 +0100

    Update theming.sh

--- scripts/installer/theming.sh
+++ scripts/installer/theming.sh
@@ -20,14 +20,14 @@ run_command "pacman -S --noconfirm qt5ct qt6ct kvantum" "Install Qt5/6 Settings 
 run_command "pacman -S --noconfirm ttf-jetbrains-mono ttf-fira-code ttf-cascadia-code" "Install popular fonts" "yes"
 
 # 5. Extract GTK + Icon themes
-run_command "tar -xvf /home/$SUDO_USER/simple-hyprland/assets/themes/Catppuccin-Mocha.tar.xz -C /usr/share/themes/" "Install Catppuccin Mocha GTK theme" "yes"
-run_command "tar -xvf /home/$SUDO_USER/simple-hyprland/assets/icons/Tela-circle-dracula.tar.xz -C /usr/share/icons/" "Install Tela Circle Dracula icon theme" "yes"
+run_command "tar -xvf /home/$SUDO_USER/hypr/assets/themes/Catppuccin-Mocha.tar.xz -C /usr/share/themes/" "Install Catppuccin Mocha GTK theme" "yes"
+run_command "tar -xvf /home/$SUDO_USER/hypr/assets/icons/Tela-circle-dracula.tar.xz -C /usr/share/icons/" "Install Tela Circle Dracula icon theme" "yes"
 
 # 6. Kvantum theme
 run_command "yay -S --sudoloop --noconfirm kvantum-theme-catppuccin-git" "Install Catppuccin Kvantum theme" "yes" "no"
 
 # 7. Kitty config
-run_command "cp -r /home/$SUDO_USER/simple-hyprland/configs/kitty /home/$SUDO_USER/.config/" "Copy Kitty Catppuccin theme config" "yes" "no"
+run_command "cp -r /home/$SUDO_USER/hypr/configs/kitty /home/$SUDO_USER/.config/" "Copy Kitty Catppuccin theme config" "yes" "no"
 
 # 8. Catppuccin SDDM Theme
 run_command "yay -S --sudoloop --noconfirm qt6-svg qt6-declarative qt5-quickcontrols2 qt5-graphicaleffects" "Install Qt dependencies for SDDM theming" "yes" "no"
