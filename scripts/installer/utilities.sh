ce97b8356ffd29045e172b89dfc300b2fbe738ee
Saltyfunnel <gerardsheal@gmail.com>
2025-07-03 08:55:31 +0100

    Update utilities.sh

--- scripts/installer/utilities.sh
+++ scripts/installer/utilities.sh
@@ -17,11 +17,11 @@ fi
 
 ### Waybar
 run_command "pacman -S --noconfirm waybar" "Install Waybar - Status Bar" "yes"
-run_command "cp -r /home/$SUDO_USER/simple-hyprland/configs/waybar /home/$SUDO_USER/.config/" "Copy Waybar config" "yes" "no"
+run_command "cp -r /home/$SUDO_USER/hypr/configs/waybar /home/$SUDO_USER/.config/" "Copy Waybar config" "yes" "no"
 
 ### Tofi
 run_command "yay -S --sudoloop --noconfirm tofi" "Install Tofi - Application Launcher" "yes" "no"
-run_command "cp -r /home/$SUDO_USER/simple-hyprland/configs/tofi /home/$SUDO_USER/.config/" "Copy Tofi config(s)" "yes" "no"
+run_command "cp -r /home/$SUDO_USER/hypr/configs/tofi /home/$SUDO_USER/.config/" "Copy Tofi config(s)" "yes" "no"
 
 ### Cliphist
 run_command "pacman -S --noconfirm cliphist" "Install Cliphist - Clipboard Manager" "yes"
@@ -31,7 +31,7 @@ run_command "yay -S --sudoloop --noconfirm swww" "Install SWWW for wallpaper man
 
 # Ensure backgrounds folder exists before copy
 run_command "mkdir -p /home/$SUDO_USER/.config/assets/backgrounds" "Create backgrounds directory" "no" "no"
-run_command "cp -r /home/$SUDO_USER/simple-hyprland/assets/backgrounds/* /home/$SUDO_USER/.config/assets/backgrounds/" "Copy background images" "yes" "no"
+run_command "cp -r /home/$SUDO_USER/hypr/assets/backgrounds/* /home/$SUDO_USER/.config/assets/backgrounds/" "Copy background images" "yes" "no"
 
 ### Hyprpicker
 run_command "yay -S --sudoloop --noconfirm hyprpicker" "Install Hyprpicker - Color Picker" "yes" "no"
@@ -39,21 +39,13 @@ run_command "yay -S --sudoloop --noconfirm hyprpicker" "Install Hyprpicker - Col
 ### Hyprlock
 run_command "yay -S --sudoloop --noconfirm hyprlock" "Install Hyprlock - Screen Locker (Must)" "yes" "no"
 run_command "mkdir -p /home/$SUDO_USER/.config/hypr" "Ensure Hypr config dir exists" "no" "no"
-run_command "cp -r /home/$SUDO_USER/simple-hyprland/configs/hypr/hyprlock.conf /home/$SUDO_USER/.config/hypr/" "Copy Hyprlock config" "yes" "no"
-
-### Wlogout
-run_command "yay -S --sudoloop --noconfirm wlogout" "Install Wlogout - Session Manager" "yes" "no"
-run_command "cp -r /home/$SUDO_USER/simple-hyprland/configs/wlogout /home/$SUDO_USER/.config/" "Copy Wlogout config" "yes" "no"
-
-# Ensure wlogout assets folder exists and copy contents properly
-run_command "mkdir -p /home/$SUDO_USER/.config/assets/wlogout" "Create wlogout assets dir" "no" "no"
-run_command "cp -r /home/$SUDO_USER/simple-hyprland/assets/wlogout/* /home/$SUDO_USER/.config/assets/wlogout/" "Copy Wlogout assets" "yes" "no"
+run_command "cp -r /home/$SUDO_USER/hypr/configs/hypr/hyprlock.conf /home/$SUDO_USER/.config/hypr/" "Copy Hyprlock config" "yes" "no"
 
 ### Grimblast
 run_command "yay -S --sudoloop --noconfirm grimblast" "Install Grimblast - Screenshot tool" "yes" "no"
 
 ### Hypridle
 run_command "yay -S --sudoloop --noconfirm hypridle" "Install Hypridle for idle management (Must)" "yes" "no"
-run_command "cp -r /home/$SUDO_USER/simple-hyprland/configs/hypr/hypridle.conf /home/$SUDO_USER/.config/hypr/" "Copy Hypridle config" "yes" "no"
+run_command "cp -r /home/$SUDO_USER/hypr/configs/hypr/hypridle.conf /home/$SUDO_USER/.config/hypr/" "Copy Hypridle config" "yes" "no"
 
 echo "------------------------------------------------------------------------"
