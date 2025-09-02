889a4fc11661e7a24bc5a33b730592207b715c4d
Saltyfunnel <gerardsheal@gmail.com>
2025-09-02 19:20:21 +0100

    Delete scripts/launch-yazi.sh

--- scripts/launch-yazi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Ensure proper Wayland environment
-export XDG_SESSION_TYPE=wayland
-export WAYLAND_DISPLAY="$WAYLAND_DISPLAY"
-export XDG_RUNTIME_DIR="$XDG_RUNTIME_DIR"
-
-# Launch Yazi as the current user
-sudo -u "$USER" setsid yazi >/dev/null 2>&1 &
