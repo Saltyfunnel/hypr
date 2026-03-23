4ec8073460db6a88a5fa5cb5b61778341fdee11a
Saltyfunnel <gerardsheal@gmail.com>
2026-03-23 14:31:27 +0000

    Update launch.sh

--- configs/rofi/launch.sh
+++ configs/rofi/launch.sh
@@ -1,10 +1,8 @@
-configuration {
-    modi:           "drun,run,window";
-    show-icons:     true;
-    icon-theme:     "Papirus-Dark";
-    font:           "Hack Nerd Font 12";
-    drun-display-format: "{name}";
-    hover-select:   true;
-    me-select-entry: "";
-    me-accept-entry: "MousePrimary";
-}
+#!/bin/bash
+WALL=$(cat ~/.cache/wal/wal)
+BG=$(grep "^    background:" ~/.cache/wal/colors-rofi-dark.rasi | awk '{print $2}' | tr -d ';#')
+
+sed -i "s|background-image:.*|background-image:            url(\"$WALL\", height);|" ~/.config/rofi/launcher.rasi
+sed -i "s|background-color:.*\/\* window-bg \*\/|background-color:            #DD${BG}; /* window-bg */|" ~/.config/rofi/launcher.rasi
+
+rofi -show drun -theme ~/.config/rofi/launcher.rasi
